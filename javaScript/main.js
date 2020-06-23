d0e8593f56b214008383b6c40556f8220e5f6a01
Shushan907 <shushan907@gmail.com>
2020-06-24 02:05:27 +0400

    add td elements

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,5 +1,5 @@
-import {drawCanvas} from './canvasDiagram.js';
-import {drawCircle} from './canvasCircle.js'
+import { drawCanvas } from './canvasDiagram.js';
+import { drawCircle } from './canvasCircle.js';
 
 drawCanvas(150, '#01AAAA');
 drawCanvas(175, '#FBCF71');
@@ -8,3 +8,15 @@ drawCanvas(200, '#1F7BB6');
 drawCircle('#01AAAA', 50, 8);
 drawCircle('#FBCF71', 40, 20);
 drawCircle('#1F7BB6', 55, 0);
+
+
+
+/*function fillTable() {
+    let i = 1;
+    let j = 0;
+    let table = document.getElementById("table");
+    for (j; j < 7; j++) {
+        let row = table.insertRow(i);
+        row.insertCell(j).innerHTML = "NEW CELL1";
+    }
+}*/
