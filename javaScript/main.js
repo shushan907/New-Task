6bd626d5edacf1aba97d47b0891f04ff7bbfdc50
Shushan907 <shushan907@gmail.com>
2020-06-24 18:54:36 +0400

    add eventListeners

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,9 +1,9 @@
 import { drawCanvas } from './canvasDiagram.js';
 import { drawCircle } from './canvasCircle.js';
 
-drawCanvas(150, '#01AAAA');
-drawCanvas(175, '#FBCF71');
-drawCanvas(200, '#1F7BB6');
+drawCanvas(150,'#01AAAA',);
+drawCanvas(175,'#FBCF71');
+drawCanvas(200,'#1F7BB6');
 
 drawCircle('#01AAAA', 50, 8);
 drawCircle('#FBCF71', 40, 20);
@@ -13,8 +13,8 @@ fillTable('affTable');
 fillTable('signTable');
 fillTable('commisionTable');
 
-let h = document.body.scrollHeight;
-document.getElementById('tools_item').style.height = h +'px';
+let height = document.body.scrollHeight;
+document.getElementById('tools_item').style.height = height +'px';
 
 async function fillTable(id) {
     let i = 1;
@@ -32,3 +32,27 @@ async function fillTable(id) {
         i++;
     }; 
 };
+//-------------------------Event Listeners----------------------------
+document.querySelector('.cliks').addEventListener('mouseover', () => {
+    drawCanvas(175,'#2A2A2A');   
+});
+
+document.querySelector('.cliks').addEventListener('mouseout', () => {
+    drawCanvas(175,'#FBCF71');
+});
+
+document.querySelector('.signUp').addEventListener('mouseover', () => {
+    drawCanvas(150,'#2A2A2A');   
+});
+
+document.querySelector('.signUp').addEventListener('mouseout', () => {
+    drawCanvas(150,'#01AAAA',);
+});
+
+document.querySelector('.pageView').addEventListener('mouseover', () => {
+    drawCanvas(200,'#2A2A2A');   
+});
+
+document.querySelector('.pageView').addEventListener('mouseout', () => {
+    drawCanvas(200,'#1F7BB6');
+});
